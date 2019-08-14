ba317dc32ba518f5cbf1da64b941ce027b2de23f
Antony <antony@mail.com>
2019-08-14 15:42:38 +0300

    collections, DataProvider - done

--- src/test/java/PO/HomePage.java
+++ src/test/java/PO/HomePage.java
@@ -11,18 +11,16 @@ public class HomePage extends BasePage {
         super(driver);
     }
 
-    By homePageHeader = By.xpath("//h1");
-    By eveningCoursesBtn = By.id("menu-item-411");
-    By eveningCoursesItem = By.id("menu-item-7880");
-    By dayCoursesBtn = By.id("menu-item-7901");
-    By daycoursesItem = By.id("menu-item-412");
+    By homePageHeader = By.xpath("(//h1[contains(text(), 'Мы - ITEA')])[1]");
+    By eveningCoursesBtn = By.xpath("(//a[contains(text(), 'Вечерние курсы')])[1]");
+    By eveningCoursesItem = By.xpath("(//a[contains(text(), 'Курсы')])[1]");
+    By dayCoursesBtn = By.xpath("(//a[contains(text(), 'Дневное обучение')])[1]");
+    By daycoursesItem = By.xpath("(//a[contains(text(), 'Курсы')])[2]");
+    By footerID = By.xpath("//*[@id='footer']");
+    By priceHolder = By.xpath("//li[@class='r-total-price-wrapper']//span");
 
     public HomePage isShown() {
         logger.info("Open Home Page");
-        /*logger.debug("debug message");
-        logger.warn("warn message");
-        logger.error("error message");
-        logger.fatal("fatal message");*/
         driver.manage().window().maximize();
         driver.get(PropertyLoader.getProperty("url"));
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(homePageHeader));
@@ -41,11 +39,9 @@ public class HomePage extends BasePage {
         Actions action = new Actions(driver);
         wait.until(ExpectedConditions.visibilityOfElementLocated(eveningCoursesBtn));
         action.moveToElement(driver.findElement(eveningCoursesBtn)).perform();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(eveningCoursesItem));
+        wait.until(ExpectedConditions.elementToBeClickable(eveningCoursesItem));
         driver.findElement(eveningCoursesItem).click();
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("(//h2)[23]")));
-        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#course h2")));
-        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"course\"]/div//h2")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(footerID));
         return this;
     }
 
@@ -54,9 +50,26 @@ public class HomePage extends BasePage {
         Actions action = new Actions(driver);
         wait.until(ExpectedConditions.visibilityOfElementLocated(dayCoursesBtn));
         action.moveToElement(driver.findElement(dayCoursesBtn)).perform();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(daycoursesItem));
+        wait.until(ExpectedConditions.elementToBeClickable(daycoursesItem));
         driver.findElement(daycoursesItem).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(footerID));
         return this;
+    }
+
+    public HomePage openCourse(String name) {
+        logger.info(String.format("Open course '%s'", name));
+        By course = By.xpath(String.format("//h2[text()='%s']/..//a[text()='Просмотреть']", name));
+        wait.until(ExpectedConditions.elementToBeClickable(course));
+        driver.findElement(course).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-filter='.course-list']")));
+        return this;
+    }
+
+    public int getCoursePrice() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(priceHolder));
+        String rawPrice = driver.findElement(priceHolder).getText();
+        int price = Integer.parseInt(rawPrice.substring(0, rawPrice.indexOf(" ")));
+        return price;
     }
 
     public HomePage Close() {
--- src/test/java/Test/GenTest.java
+++ src/test/java/Test/GenTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import test.java.PO.ContactPage;
 import test.java.PO.DayCoursesPage;
@@ -54,7 +55,7 @@ public class GenTest {
     }
 
     @Test(description = "check evening courses are present")
-    public void EveningCoursesCheck() {
+    public void eveningCoursesCheck() {
         homePage.isShown();
         homePage.openEveningCoursesPage();
 
@@ -62,11 +63,51 @@ public class GenTest {
     }
 
     @Test(description = "check day courses are present")
-    public void DayCoursesCheck() {
+    public void dayCoursesCheck() {
         homePage.isShown();
         homePage.openDayCoursesPage();
 
         assertTrue(dayCoursesPage.checkDayCoursesArePresent());
+    }
+
+    @Test(description = "Check courses price", dataProvider = "coursesProvider")
+    public void checkCoursesPrice(String courseName, int expectedPrice) {
+        homePage.isShown();
+        homePage.openEveningCoursesPage();
+        homePage.openCourse(courseName);
+         //       .openNigthCourses()
+          //      .openCourse(courseName);
+        int actualPrice = homePage.getCoursePrice();
+        assertEquals(actualPrice, expectedPrice, String.format("Expected price to be equals '%d' for '%s'", expectedPrice, courseName));
+    }
+
+    @DataProvider(name = "coursesProvider")
+    public Object[][] coursesProvider() {
+        return new Object[][] {
+                {"Тестирование", 17300},
+                {"Frontend development", 18100},
+                {"JS development", 45200},
+                {"Веб-дизайн", 27700},
+                {"PHP", 16550},
+                {"Программирование под IOS", 20350},
+                {"Программирование под Android", 30200},
+                {"Java programming", 25800},
+                {"Python", 30600},
+                {"Data Science/Machine Learning", 30350},
+                {"C# /.NET development", 14050},
+                {"C++", 15650},
+                {"Game Development", 10300},
+                {"DEVOPS", 16850},
+                {"Digital Marketing", 28150},
+                {"Управление персоналом", 10800},
+                {"Управление проектами", 31050},
+                {"Менеджмент", 5950},
+                {"Кибербезопасность", 4000},
+                {"Mobile development", 47500},
+                {"Видеомонтаж", 4400},
+                {"Cisco", 24000},
+                {"Go development", 11700}
+        };
     }
 
     @Test(description = "debug test for fail result")
