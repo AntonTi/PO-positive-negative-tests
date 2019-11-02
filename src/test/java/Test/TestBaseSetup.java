b439ff8fba2f0f60752234bbf468edd19f5e989f
Antony <antony@mail.com>
2019-11-02 15:43:35 +0200

    added class TestBaseSetup

---
+++ src/test/java/Test/TestBaseSetup.java
@@ -0,0 +1,26 @@
+package test.java.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBaseSetup {
+
+    WebDriver driver;
+
+    @BeforeMethod(description = "Initialize custom driver")
+    public void init(ITestContext context) {
+        System.setProperty("webdriver.chrome.driver"
+                , "D:\\Download\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        context.setAttribute("webDriver", driver);
+    }
+
+    @AfterMethod(description = "Finalize custom driver")
+    public void finalize() {
+        driver.quit();
+    }
+
+}
